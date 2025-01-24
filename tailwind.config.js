27aa059e16cfadb1bbbf6ac79bf76ced61b895e6
7juned7 <96330909+7juned7@users.noreply.github.com>
2025-01-24 23:25:35 +0530

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,15 @@ export default {
       },
     },
   },
+  safelist: [
+    'bg-red-500',
+    'bg-green-500',
+    'bg-yellow-500',
+    'bg-pink-500',
+    'text-red-500',
+    'text-green-500',
+    'text-yellow-500',
+    'text-pink-500',
+  ],
   plugins: [],
 };
