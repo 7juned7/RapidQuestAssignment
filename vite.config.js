4bae228b250dd6c49fbabc266cfea46e012cd51f
7juned7 <96330909+7juned7@users.noreply.github.com>
2025-01-24 21:22:14 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://rapidquestassignment.onrender.com',
+        target: 'https://rapidquestassignment.onrender.com', // Backend URL
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove "/api" prefix
       },
     },
   },
